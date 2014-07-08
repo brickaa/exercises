c7a3d34f78cd094ea18fa1674f135079d7c55a94
brickaa <becca.aaronson@Gmail.com>
2014-07-07 20:02:09 -0500

    working rps code

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -2,17 +2,48 @@ console.log('script.js loaded');
 
 //Computer's choice
 
-function computerChoice () {
+function computerChoice() {
 	var number = Math.random();
 	if (number >= 0 && number <= 0.3333333333) {
-		return "Rock";
+		return "rock";
 	} else if (number <= 0.66666666666) {
-		return "Paper";
+		return "paper";
 	} else {
-		return "Scissors";
+		return "scissors";
 	}
 }
-console.log(computerChoice());
+
 //User's choice
+$(".user").on('click', function(){
+	var computer = computerChoice();
+	var user = this.id;
+	var result = compare(computer, user);
+	$("#result").html(result);
+	$("#computer").html(computer);
+	$("#user").html(user);
+});
 
 //Compare: Computer v. User
+function compare(x, y) {
+	if (x == y) {
+		return "It's a tie!";
+	} else if (x === "rock") {
+		if (y === "scissors") {
+			return "Computer wins.";
+		} else {
+			return "You win!";
+		}
+	} else if (x === "scissors") {
+		if (y === "paper") {
+			return "Computer wins.";
+		} else {
+			return "You win!";
+		}
+	} else {
+		if (y === "rock") {
+			return "Computer wins.";
+		} else {
+			return "You win!";
+		}
+	}
+}
